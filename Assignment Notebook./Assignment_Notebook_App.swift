71ebb182756e00bfba2756b3e2df36ed44a2155a
chris robles <croblesquezad@bsd220.org>
2024-02-08 09:32:28 -0600

    Initial Commit

---
+++ Assignment Notebook./Assignment_Notebook_App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_App.swift
+//  Assignment Notebook.
+//
+//  Created by christopher robles quezada on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
